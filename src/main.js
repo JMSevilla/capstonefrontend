f81892cb31a473f8acde4f24c229ddc0d7217f57
Sync Method Development <miggysvll@gmail.com>
2021-04-25 16:11:50 +0800

    capstone

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import elemento from 'element-ui';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.config.productionTip = false
-
+Vue.use(elemento);
 new Vue({
   router,
   store,
---
+++ src/views/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <navbar />
+    </div>
+</template>
+
+<script>
+import navbar from "@/components/navigation/navbar";
+export default {
+    components: {
+        navbar
+    }
+}
+</script>
